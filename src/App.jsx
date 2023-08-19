e2d8db9243f26f1f3d921f9f987a4d09b8c78a87
Lynn-Hsiao <lynnlynnlynn935@gmail.com>
2023-08-19 19:39:40 +0800

    Setting Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { LoginPage } from 'pages';
 import './App.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { TodoPage, LoginPage, SignUpPage } from './pages';
 
 function App() {
   return (
     <div className="app">
-      <TodoPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="signUp" element={<SignUpPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="todo" element={<TodoPage />} />
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
